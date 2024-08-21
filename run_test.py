7c41891a2bf23e73ed9be4dcf9551f81a27b681c
jaehyeok.jeong <jaehyeok.jeong@konantech.com>
2024-08-21 14:16:52 +0900

    add gitignore

--- run_test.py
+++ run_test.py
@@ -1,8 +1,8 @@
 from project.make_project import make_attirbution
-project = make_attirbution('./config_darknet.yaml')
+# project = make_attirbution('./config_darknet.yaml')
 # project = make_attirbution('./config_yolo.yaml')
 # project = make_attirbution('./config_resnet_lrp.yaml')
 # project = make_attirbution('./config_vgg_lrp.yaml')
 # project = make_attirbution('./config_efficientnet_lrp.yaml')
-# project = make_attirbution('./config_yolo_lrp.yaml')
+project = make_attirbution('./config_yolo_lrp.yaml')
 project.run()
