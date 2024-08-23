6de4eaf655134486a02d67343a2e0e4b73c85de4
jaehyeok.jeong <jaehyeok.jeong@konantech.com>
2024-08-23 18:20:44 +0900

    branch test

--- tutorial_with_project.py
+++ tutorial_with_project.py
@@ -7,8 +7,8 @@ from project.make_project import Project
 # config_path = './project/config_resnet_gradcam.yaml'
 # config_path = './project/config_vgg_lrp.yaml'
 # config_path = './project/config_vgg_gradcam.yaml'
-# config_path = './project/config_yolo_lrp.yaml'
-config_path = './project/config_yolo.yaml'
+config_path = './project/config_yolo_lrp.yaml'
+# config_path = './project/config_yolo.yaml'
 
 project = Project(config_path)
 project.run()
