f89f75e5c1cdff04ee11fe91f643bfe51e995800
jaehyeok.jeong <jaehyeok.jeong@konantech.com>
2024-08-23 18:11:04 +0900

    make dev branch

--- tutorial_with_project.py
+++ tutorial_with_project.py
@@ -1,6 +1,6 @@
 from project.make_project import Project
 
-config_path = './project/config_darknet.yaml'
+# config_path = './project/config_darknet.yaml'
 # config_path = './project/config_efficientnet_lrp.yaml'
 # config_path = './project/config_efficientnet_gradcam.yaml'
 # config_path = './project/config_resnet_lrp.yaml'
@@ -8,7 +8,7 @@ config_path = './project/config_darknet.yaml'
 # config_path = './project/config_vgg_lrp.yaml'
 # config_path = './project/config_vgg_gradcam.yaml'
 # config_path = './project/config_yolo_lrp.yaml'
-# config_path = './project/config_yolo.yaml'
+config_path = './project/config_yolo.yaml'
 
 project = Project(config_path)
 project.run()
