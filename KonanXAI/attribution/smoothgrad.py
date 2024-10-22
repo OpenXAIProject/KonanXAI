done correct for gradient and still correcting smoothgrad yolo

KonanXAI/attribution/smoothgrad.py
@@ -32,11 +32,13 @@ class SmoothGrad(Gradient):
         self.std = self.std * (torch.max(self.input) - torch.min(self.input))
         noise_sampling = gaussian_noise(self.input.shape, self.std, self.sample_size).to(self.device)
         samples = torch.repeat_interleave(self.input, self.sample_size, dim = 0)
-        self.input = samples + self.noise_level * noise_sampling
+        self.inputs = samples + self.noise_level * noise_sampling
         
     def calculate(self):
         self._gaussian_noise_sample()
-        self.get_saliency()
+        for i in range(self.sample_size):
+            self.input = self.inputs[i].unsqueeze(0)
+            self.get_saliency()
         if self.framework == 'torch':
             if self.model_name in ('yolov4', 'yolov4-tiny', 'yolov5s'):
                 for i, heatmap in enumerate(self.heatmaps):
@@ -54,33 +56,21 @@ class SmoothGrad(Gradient):
             param.requires_grad = True
 
         self.preds_origin, raw_logit = self.model(self.input)
-        self.logits_origin = []
-        self.preds = []
-        self.select_layers = []
-        self.index_tmep = []
-        for i in range(self.preds_origin.shape[0]):
-            logits_origin = torch.concat([data.view(-1,self.preds_origin[i].shape[-1])[...,5:] for data in raw_logit[0][i]],dim=0)
-            self.logits_origin.append(logits_origin)
-        #self.logits_origin = torch.concat([data.view(-1,self.preds_origin.shape[-1])[...,5:] for data in raw_logit],dim=0)
-        
-            with torch.no_grad():
-                preds, logits, select_layers = non_max_suppression(self.preds_origin[i], self.logits_origin[0][i], conf_thres=0.25, model_name = self.model_name)
-                self.preds.append(preds)
-                self.select_layers.append(select_layers)
-                index_tmep = yolo_choice_layer(raw_logit[i], self.select_layers[i])
-                self.index_tmep.append(index_tmep)
+        self.logits_origin = torch.concat([data.view(-1,self.preds_origin.shape[-1])[...,5:] for data in raw_logit],dim=0)
+        with torch.no_grad():
+            self.preds, logits, self.select_layers = non_max_suppression(self.preds_origin, self.logits_origin.unsqueeze(0), conf_thres=0.25, model_name = self.model_name)
+        self.index_tmep = yolo_choice_layer(raw_logit, self.select_layers)
 
     def _yolo_backward_pytorch(self):
         self.bboxes = []
         self.heatmaps = []
         for cls, sel_layer, sel_layer_index in zip(self.preds[0], self.select_layers, self.index_tmep):
             self.model.zero_grad()
+            if self.input.grad != None:
+                self.input.grad.zero_()
             self.logits_origin[sel_layer][int(cls[5].item())].backward(retain_graph=True)
 
-            heatmap = self.input.grad
+            heatmap = self.input.grad.clone().detach()
             self.heatmaps.append(heatmap)
             self.bboxes.append(cls[...,:4].detach().cpu().numpy())
             
-               
-
-    