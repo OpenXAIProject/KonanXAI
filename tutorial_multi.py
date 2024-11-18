dc94399172b5cde688f61bcc3b2b19e2a5af1a64
jaehyeok.jeong <jaehyeok.jeong@konantech.com>
2024-11-18 13:37:02 +0900

    multi inference list -> dict test complete

--- tutorial_multi.py
+++ tutorial_multi.py
@@ -1,5 +1,5 @@
 from KonanXAI import autoXAI, preprocessing
-json_path = "D:/xai_refactoring/piper_test_yaml/evaluation/resnet_multi_test.json"
+json_path = "D:/xai_refactoring/piper_test_yaml/evaluation/resnet_multi_test_dict.json"
 job_type, model_param, datasets_param, arg_param, output = preprocessing(json_path)
 
 work_handle = autoXAI.run_xai(job_type, model_param, datasets_param, arg_param, output)
